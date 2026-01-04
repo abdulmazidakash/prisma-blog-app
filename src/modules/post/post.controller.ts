08ccd706da51f82fdc179ae5dcff7d3701bdd371
akashabdulmazid <akashabdulmazid@gmail.com>
2026-01-04 18:54:43 +0600

    module 25 complete

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { PostService } from "./post.service";
+import { PostStatus } from "../../../generated/prisma/enums";
 
 const createPost = async (req: Request, res: Response) => {
     // res.send('create a new post');
@@ -25,11 +26,31 @@ const createPost = async (req: Request, res: Response) => {
     }
 };
 
-const getAllPost = async(req: Request, res: Response)=>{
+const getAllPost = async (req: Request, res: Response) => {
     try {
-        const result = await PostService.getAllPost();
+        const { search } = req.query;
+        // console.log('Search Value: ', search);
+
+        const searchString = typeof search === 'string' ? search : undefined;
+
+        const tags = req.query.tags ? (req.query.tags as string).split(',') : [];
+
+        const isFeatured = req.query.isFeatured
+            ? req.query.isFeatured === 'true'
+                ? true
+                : req.query.isFeatured === 'false'
+                    ? false
+                    : undefined
+            : undefined;
+
+        console.log(isFeatured)
+        const status = req.query.status as PostStatus | undefined;
+
+        const authorId = req.query.authorId as string | undefined;
+
+        const result = await PostService.getAllPost({ search: searchString, tags, isFeatured, status, authorId });
         res.status(200).json(result);
-        
+
     } catch (error) {
         res.status(400).json({
             error: "Failed to fetch posts",
--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,7 +1,8 @@
-import { Post } from "../../../generated/prisma/client";
+import { Post, PostStatus } from "../../../generated/prisma/client";
+import { PostWhereInput } from "../../../generated/prisma/models";
 import { prisma } from "../../lib/prisma";
 
-const createPost = async(data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">, userId: string)=>{
+const createPost = async (data: Omit<Post, "id" | "createdAt" | "updatedAt" | "authorId">, userId: string) => {
     const result = await prisma.post.create({
         data: {
             ...data,
@@ -11,9 +12,73 @@ const createPost = async(data: Omit<Post, "id" | "createdAt" | "updatedAt" | "au
     return result;
 };
 
-const getAllPost = async()=>{
+const getAllPost = async ({search,tags, isFeatured, status, authorId}: {
+    search: string | undefined,
+    tags: string[] | [],
+    isFeatured: boolean | undefined,
+    status: PostStatus | undefined,
+    authorId: string | undefined,
+
+}) => {
     // console.log('get all post');
-    const allPost = await prisma.post.findMany();
+
+    const andConditions: PostWhereInput[] = [];
+
+    if (search) {
+        andConditions.push({
+            OR: [
+                {
+                    title: {
+                        contains: search,
+                        mode: "insensitive",
+                    }
+                },
+                {
+                    content: {
+                        contains: search,
+                        mode: "insensitive",
+                    }
+                },
+                {
+                    tags: {
+                        has: search,
+                    }
+                }
+            ]
+        },)
+    };
+
+    if(tags.length > 0){
+        andConditions.push({
+            tags:{
+                hasEvery: tags as string[],
+            }
+        })
+    };
+
+    if(typeof isFeatured === 'boolean'){
+        andConditions.push({
+            isFeatured
+        })
+    };
+
+    if(status){
+        andConditions.push({
+            status
+        })
+    };
+
+    if(authorId){
+        andConditions.push({
+            authorId
+        })
+    }
+
+    const allPost = await prisma.post.findMany({
+        where: {
+            AND: andConditions
+        }
+    });
     return allPost;
 }
 
